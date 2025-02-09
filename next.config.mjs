007c6234a2908876df9bb08d2d9d5b685991590e
AhmedElhor7 <ahmedelhor446@gmail.com>
2025-02-09 21:38:13 +0200

    Configured export and build for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  distDir: 'dist',
+  distDir: "dist",
+  basePath: "/elveatetest", // Add your GitHub repository name here
+  trailingSlash: true,
   images: {
+    unoptimized: true, // Necessary for GitHub Pages since it does not support optimized images
     remotePatterns: [
       {
         protocol: "https",
         hostname: "ecommerce.routemisr.com",
         port: "",
         pathname: "/**",
-        search: "",
       },
     ],
   },
